5505483b38b9bff56d9e4057522eb7286b7813e9
notomo <notomo.motono@gmail.com>
2022-12-30 09:41:58 +0900

    Small refactor error handling

--- lua/filetypext/command.lua
+++ lua/filetypext/command.lua
@@ -1,6 +1,6 @@
-local ReturnValue = require("filetypext.vendor.misclib.error_handler").for_return_value()
+local M = {}
 
-function ReturnValue.detect(raw_opts)
+function M.detect(raw_opts)
   local opts = require("filetypext.core.option").new(raw_opts)
   return require("filetypext.core.detect").file_names(
     opts.base_name,
@@ -10,4 +10,4 @@ function ReturnValue.detect(raw_opts)
   )
 end
 
-return ReturnValue:methods()
+return M
