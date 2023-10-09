2ed8cdf784ae005508bc9c6c6a0a94aac5fd1380
notomo <notomo.motono@gmail.com>
2023-10-09 10:05:06 +0900

    Small refactor

--- lua/filetypext/test/helper.lua
+++ lua/filetypext/test/helper.lua
@@ -1,17 +1,11 @@
 local helper = require("vusted.helper")
 local plugin_name = helper.get_module_root(...)
 
-helper.root = helper.find_plugin_root(plugin_name)
-
 function helper.before_each() end
 
 function helper.after_each()
   helper.cleanup()
   helper.cleanup_loaded_modules(plugin_name)
-end
-
-function helper.set_lines(lines)
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(lines, "\n"))
 end
 
 return helper
